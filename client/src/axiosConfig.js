351792f7daa4280819c80db92f1e1bd6f86a40e8
Sai Ganesh <157967804+Ganesh-Tammana@users.noreply.github.com>
2024-07-29 18:51:40 +0530

    Update axiosConfig.js

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,1 +1,9 @@
+// src/axiosConfig.js
+import axios from 'axios';
 
+const instance = axios.create({
+  baseURL: 'https://gemini-mind-api.onrender.com',
+  withCredentials: true
+});
+
+export default instance;
