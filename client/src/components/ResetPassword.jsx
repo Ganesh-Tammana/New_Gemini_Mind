3b753cd69023d340a44c8be0994bf26743cefe2b
Sai Ganesh <157967804+Ganesh-Tammana@users.noreply.github.com>
2024-07-29 18:57:58 +0530

    Update ResetPassword.jsx

--- client/src/components/ResetPassword.jsx
+++ client/src/components/ResetPassword.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useFormik} from 'formik';
-import axios from ../axiosConfig.js'
+import axios from ../axiosConfig'
 import * as Yup from 'yup';
 import {useNavigate,Link,useParams} from 'react-router-dom';
 
