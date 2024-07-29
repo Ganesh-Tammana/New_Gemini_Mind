301c88857f032d14ccb86e8a095a2c71b061fccb
Sai Ganesh <157967804+Ganesh-Tammana@users.noreply.github.com>
2024-07-29 18:25:34 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,22 +10,11 @@ const PORT=process.env.PORT;
 const app=express();
 app.use(cookieParser());
 
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://gemini-mind.onrender.com'
-];
-
 app.use(cors({
-   origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
+   origin:"*",
+   credentials:true
 }));
+
 app.use(express.json());
 app.use('/auth',UserRouter);
 const passKEY=process.env.MONGODB
